88311e249b9a7db6aceda4fef8b95ff30605ea1f
Kevin Monisit <monisitkevin@gmail.com>
2022-04-23 00:52:31 -0400

    Initial setup

---
+++ backend/app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var path = require('path');
+var cors = require('cors');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./api/index');
+var usersRouter = require('./api/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
