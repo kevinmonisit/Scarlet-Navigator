476876651425e8559839b72a73a8b1f7b2dff159
Kevin Monisit <monisitkevin@gmail.com>
2022-05-04 15:10:27 -0400

    Cleaning up files

--- backend/models/requirement.ts
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const RequirementSchema = new Schema({
-  title: String,
-  courses: [{ type: Schema.Types.ObjectId, ref: 'Course' }],
-});
-
-export {};
--- backend/populatedb.ts
+++ backend/populatedb.ts
@@ -1,58 +1,141 @@
-/* eslint-disable no-console */
-const mongoose = require('mongoose');
-const User = require('./models/user.ts');
-const Course = require('./models/course.ts');
+/* eslint-disable no-underscore-dangle */
+import mongoose, { Schema, HydratedDocument } from 'mongoose';
+import async from 'async';
+import dotenv from 'dotenv';
+import UserModel, { User } from './models/UserModel';
+import CourseModel, { Course } from './models/CourseModel';
+
+const coursesArray: Array<Schema.Types.ObjectId> = [];
+
+dotenv.config();
+
+function createUser(
+  courses: Array<Schema.Types.ObjectId>,
+  startingYear: Number,
+  plan: Array<Array<Schema.Types.ObjectId>>,
+  callback: CallableFunction
+) {
+  const userDocument: HydratedDocument<User> = new UserModel({
+    courses,
+    startingYear,
+    plan,
+  });
+  userDocument.save((err: any) => {
+    if (err) console.warn(err);
+    console.log('User created');
+    callback(null);
+  });
+}
+
+function createCourse(
+  title: String,
+  credits: Number,
+  prerequisites: Array<String>,
+  callback: CallableFunction
+) {
+  console.log(`Creating course ${title}`);
+  const courseDocument: HydratedDocument<Course> = new CourseModel({
+    title,
+    credits,
+    prerequisites,
+  });
+  courseDocument.save((err: any) => {
+    if (err) console.warn(err);
+    coursesArray.push(courseDocument._id);
+    callback(null, courseDocument);
+  });
+}
+
+function createCourses(callbackOuter: CallableFunction) {
+  console.log('Creating courses');
+  async.parallel(
+    [
+      (callback) => {
+        createCourse('CS112', 2, [], callback);
+      },
+      (callback) => {
+        createCourse('CS111', 3, [], callback);
+      },
+      (callback) => {
+        createCourse('MAT205', 2, [], callback);
+      },
+      (callback) => {
+        createCourse('MAT201', 2, [], callback);
+      },
+      (callback) => {
+        createCourse('CS205', 2, [], callback);
+      },
+      (callback) => {
+        createCourse('EXPOS', 2, [], callback);
+      },
+      (callback) => {
+        createCourse('Underwater Basket Weaving', 2, [], callback);
+      },
+      (callback) => {
+        createCourse('Dance Dance Revolution', 2, [], callback);
+      },
+      (callback) => {
+        createCourse('Navigating TempleOS', 2, [], callback);
+      },
+    ],
+    (err, result) => {
+      callbackOuter(err, result);
+    }
+  );
+}
+
+function createUsers(courses: Array<Course>, callback: CallableFunction) {
+  console.log('Creating users');
+
+  const startingYear = 2022;
+  const plan: Array<Array<Schema.Types.ObjectId>> = [
+    [coursesArray[0]!, coursesArray[1]!],
+    [],
+    [coursesArray[2]!, coursesArray[3]!],
+    [coursesArray[4]!, coursesArray[5]!, coursesArray[6]!],
+    [],
+    [coursesArray[7]!],
+    [coursesArray[8]!],
+    [],
+  ];
+
+  createUser(
+    courses.map((course) => course._id),
+    startingYear,
+    plan,
+    callback
+  );
+}
 
 mongoose
-  .connect('mongodb://localhost:27017/scarlet-navigator-test', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  .connect('mongodb://localhost:27017/scarlet-navigator-test')
+  .catch((err) => {
+    console.warn(err);
   })
   .then(() => {
     mongoose.connection.db.dropDatabase();
+    console.log('Connected to MongoDB database and database dropped.');
+
+    async.waterfall(
+      [
+        (callback: CallableFunction) => {
+          createCourses(callback);
+        },
+        (courses: Array<Course>, callback: CallableFunction) => {
+          createUsers(courses, callback);
+        },
+      ],
+      (err) => {
+        if (err) {
+          console.warn(err);
+        } else {
+          console.log('Successful population of database');
+        }
+        process.exit();
+      }
+    );
   });
 
 mongoose.connection.on('error', (err) => {
   console.warn(`Error: ${err}`);
 });
-
-interface CourseSchema {
-  title: String;
-  credits: Number;
-  prerequisites: CourseSchema[];
-}
-
-function CreateUser(
-  major: String,
-  courses: Array<String>,
-  startingYear: Number
-) {
-  const userDocument = new User({ major, courses, startingYear });
-  User.save(userDocument, (err) => {
-    if (err) console.warn(err);
-  });
-}
-
-function CreateCourse(
-  course: String,
-  credits: Number,
-  prerequisites: Array<String>
-) {
-  const courseDocument = new Course({ course, credits, prerequisites });
-  Course.save(courseDocument, (err) => {
-    if (err) console.warn(err);
-  });
-}
-
-CreateCourse('test #1', 2, []);
-CreateCourse('test #2', 2, []);
-CreateCourse('test #3', 2, []);
-CreateCourse('test #4', 2, []);
-CreateCourse('test #5', 2, []);
-CreateUser(
-  'test_user',
-  ['test #1', 'test #2', 'test #3', 'test #4', 'test #5'],
-  2
-);
-
-export {};
