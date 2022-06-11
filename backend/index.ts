ae92db25009ad684f6452e348fcda89da5affc5a
Kevin Monisit <monisitkevin@gmail.com>
2022-06-11 17:35:33 -0400

    Change cors

--- backend/index.ts
+++ backend/index.ts
@@ -42,7 +42,7 @@ app.use(morgan('tiny'));
 app.set('json spaces', 2);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use('/api/v1/user', userRouter);
 app.use('/api/v1/courses', courseRouter);
