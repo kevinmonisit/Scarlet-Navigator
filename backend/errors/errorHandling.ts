f9f1a33711d6e2fadc256cb560cb44efbf977261
Kevin Monisit <monisitkevin@gmail.com>
2022-06-10 21:56:15 -0400

    Deleted errors

--- backend/errors/errorHandling.ts
+++
@@ -1,9 +0,0 @@
-function errorLogger(error, req, res, next) {
-  if (error.type == 'redirect') {
-    res.redirect('/error');
-  } else if (error.type == 'time-out') {
-    res.status(408).send(error);
-  } else {
-    next(error);
-  }
-}
