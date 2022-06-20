db9c3d2c7e57c941f6adbdddc585d1d234daf3a7
Kevin Monisit <monisitkevin@gmail.com>
2022-06-19 21:13:44 -0400

    Able to change search type

--- backend/api/courses.ts
+++ backend/api/courses.ts
@@ -6,12 +6,33 @@ const router = express.Router();
 
 interface ReqQuery {
   search: string;
+  amount: number;
+  searchType: number;
 }
 
 router.get(
   '/',
   async (req: Request<{}, {}, {}, ReqQuery, {}>, res: Response) => {
-    const coursesQuery = await CourseController.getCourses(req.query['search']);
+    const { amount, search, searchType } = req.query;
+    if (Number.isNaN(amount) || amount <= 0 || amount > 100) {
+      res.status(400).send({
+        error: 'amount parameter should be a number, x, where 0 <= x <= 100',
+      });
+      return;
+    }
+
+    if (searchType > 2 || searchType < 0) {
+      res.status(400).send({
+        error: `SearchType is invalid. Value is ${searchType}`,
+      });
+      return;
+    }
+
+    const coursesQuery = await CourseController.getCourses(
+      search,
+      amount,
+      searchType
+    );
     res.status(200).send({ coursesQuery });
   }
 );
--- backend/controllers/courseController.ts
+++ backend/controllers/courseController.ts
@@ -4,12 +4,38 @@ import CourseModel, { Course } from '../models/CourseModel';
  * Implements course dv-level functions
  */
 
-async function getCourses(name: string) {
+// eslint-disable-next-line no-shadow
+enum SEARCH_BY {
+  TITLE = 0,
+  NUMBER = 1,
+  EXPANDED_TITLE = 2,
+}
+
+async function getCourses(
+  name: string,
+  numberToQuery: number,
+  searchType: SEARCH_BY = SEARCH_BY.TITLE
+) {
   const regexSearch = new RegExp(`${name}`);
-  const coursesQuery = await CourseModel.find({
+  const byTitle = {
     title: { $regex: regexSearch, $options: 'i' },
-  })
-    .limit(40)
+  };
+  const byCourseNumbers = {
+    courseString: { $regex: regexSearch, $options: 'i' },
+  };
+  const byExpandedTitle = {
+    queryTitle: { $regex: regexSearch, $options: 'i' },
+  };
+
+  let query: any = byTitle;
+  const searchTypeAsNumber = parseInt(searchType as unknown as string, 10);
+  if (searchTypeAsNumber === SEARCH_BY.NUMBER) {
+    query = byCourseNumbers;
+  } else if (searchTypeAsNumber === SEARCH_BY.EXPANDED_TITLE) {
+    query = byExpandedTitle;
+  }
+  const coursesQuery = await CourseModel.find(query)
+    .limit(numberToQuery)
     .exec();
   return coursesQuery;
 }
